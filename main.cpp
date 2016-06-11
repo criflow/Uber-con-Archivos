60f8e7a1134c95c89975928b0dc3a1a22d3a4f99
samirant15 <samirant15@gmail.com>
2016-06-11 18:40:08 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+typedef struct
+{
+    string marca;
+    int ano;
+    char ficha[6];
+    string propietario;
+}carro;
+
+typedef struct
+{
+    string nombre; //nombre completo
+    int id;
+    string lugarDeTrabajo;
+    int celular;
+    string companiaCel;
+}pasajero;
+
+typedef struct
+{
+    int dia;
+    int mes;
+    int ano;
+    int hora;
+    int minuto;
+    int segundo;
+}tiempo;
+
+typedef struct
+{
+    char fichaVehiculo[6];
+    int idPasajero;
+    float pago;
+    float devuelta;
+    tiempo fecha;
+}transportePrestado;
+
+int main()
+{
+    printf("----------------------------------------------------------------------------");
+    printf("\n|                    Sistema de Conchado Inteligente                       |");
+    printf("\n----------------------------------------------------------------------------");
+    printf("\n\n*Seleccione una de las siguientes opciones:");
+    printf("\n\n1. Solicitar un concho");
+    printf("\n2. Solicitar un vehiculo prestado");
+    printf("\n3. Registrar un vehiculo");
+    printf("\n4. Registrar un pasajero");
+    printf("\n5. Mostrar pasajeros que han viajado en cierto concho");
+    printf("\n6. Mostrar conchos en los cuales ha viajado cierto pasajero");
+    printf("\n\n----------------------------------------------------------------------------");
+}
