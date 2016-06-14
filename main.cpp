00ce6fc4c1c0bbf850b07f84ff6c35f018f6d13f
criflow <crisflowbg11@gmail.com>
2016-06-14 10:44:57 -0400

    Update main.cpp
    Switch cases

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstdio>
-//pepito
+
 using namespace std;
 
 typedef struct
@@ -52,4 +52,66 @@ int main()
     printf("\n5. Mostrar pasajeros que han viajado en cierto concho");
     printf("\n6. Mostrar conchos en los cuales ha viajado cierto pasajero");
     printf("\n\n----------------------------------------------------------------------------");
+
+    int opcion;
+    cin >> opcion;
+
+    switch(opcion){
+
+    case 1:{
+        int op1;
+        printf("\nSeleccione la caracteristica que quiere solicitar: ");
+        printf("\n\n1. Por Marca");
+        printf("\n2. Por Ano");
+        printf("\n3. Por Estado");
+        printf("\n4. Por nombre de chofer");
+        printf("\n5. Cualquiera");
+        cin >> op1;
+        switch(op1){
+            case 1:{
+
+
+                }
+            case 2:{
+
+
+                }
+            case 3:{
+
+
+                }
+            case 4:{
+
+
+                }
+            case 5:{
+
+
+                }
+
+            }
+        }
+
+    case 2:{
+        int op2;
+        printf("\nSeleccione la caracteristica que quiere solicitar: ");
+        printf("\n\n1. Por Marca");
+        printf("\n2. Por Ano");
+        printf("\n3. Por Estado");
+        printf("\n4. Por nombre de chofer");
+        printf("\n5. Cualquiera");
+        cin >> op2;
+
+        }
+    case 3:{
+
+        }
+    case 4:{
+
+        }
+
+    case 5: {
+
+        }
+    }
 }
