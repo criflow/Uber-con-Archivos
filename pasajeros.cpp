e0c369d12e60ab35642589fe6337cc4a6b66c993
criflow <crisflowbg11@gmail.com>
2016-06-18 02:23:30 -0400

    Update pasajeros.cpp
    hecho

--- pasajeros.cpp
+++ pasajeros.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <cstdio>
+#include <conio.h>
+#include <stdlib.h>
 #include "vehiculos.h"
 #include "pasajeros.h"
 #include "servicios.h"
@@ -6,3 +10,57 @@
 #include "servicios.h"
 
 using namespace std;
+
+pasajero Agregarpasajero()
+{
+    pasajero cliente;
+    fflush(stdin);
+    printf("\n-Nombre del pasajero: ");
+    cin >> cliente.nombre;
+    fflush(stdin);
+    printf("\n-Lugar de trabajo del pasajero: ");
+    cin >> cliente.lugarDeTrabajo;
+    fflush(stdin);
+    printf("\n-Numero de celular del pasajero: ");
+    scanf("%d", &cliente.celular);
+    fflush(stdin);
+    printf("\n-Compania de celular del pasajero: ");
+    cin >> cliente.companiaCel;
+    fflush(stdin);
+    printf("\n-Id del pasajero: ");
+    scanf("%d", &cliente.id);
+    fflush(stdin);
+
+    return cliente;
+
+}
+void AgregarPasajeros(pasajero clientes[],int tam, int cant)
+{
+    system("cls");
+    fflush(stdin);
+    printf("------------------------- pasajero/s a agregar --------------------------------");
+    for(int i=tam; i<(tam+cant); i++)
+    {
+        printf("\n*pasajero #%d: ", i+1);
+        clientes[i] = Agregarpasajero();
+    }
+    return;
+}
+
+void MostrarPasajeros(pasajero clientes[], int tam)
+{
+    system("cls");
+    printf("------------------------- pasajeros --------------------------------");
+    for(int i=0; i<tam; i++)
+    {
+        printf("\n\n*pasajero #%d: ", i+1);
+        printf("\n-Nombre del pasajero: %s", clientes[i].nombre);
+        printf("\n-Lugar de Trabajo del pasajero: %s", clientes[i].lugarDeTrabajo);
+        printf("\n-Numero celular del pasajero: %d", clientes[i].celular);
+        printf("\n-Compania de celular del pasajero: %s", clientes[i].companiaCel);
+        printf("\n-Id del pasajero: %d", clientes[i].id);
+    }
+    getch();
+    return;
+}
+
