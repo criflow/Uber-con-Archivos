c7b33fea0afe7c973bec8705d5ced782ed533453
criflow <crisflowbg11@gmail.com>
2016-06-18 02:24:54 -0400

    Update pasajeros.h
    hecho tambiena

--- pasajeros.h
+++ pasajeros.h
@@ -10,4 +10,9 @@ typedef struct
     char companiaCel[10];
 }pasajero;
 
+pasajero AgregarPasajero();
+void AgregarPasajeros(pasajero[], int, int);
+void MostrarPasajeros(pasajero cliente[], int tam);
+
+
 #endif // PASAJEROS_H_INCLUDED
