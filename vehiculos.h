14b7e88daf53e33599f25d8775745ccdd6c30252
samirant15 <samirant15@gmail.com>
2016-06-19 20:06:39 -0400

    Update vehiculos.h

--- vehiculos.h
+++ vehiculos.h
@@ -11,7 +11,7 @@ typedef struct
 
 carro AgregarCarro();
 void AgregarCarros(carro[], int, int);
-void MostrarCarros(carro conchos[], int tam);
+void MostrarCarros(carro [], int );
 bool BuscarVehiculo(carro[], int, char[]);
 
 
