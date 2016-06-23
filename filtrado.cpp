dfc8fb5214c6ba9959c8fb5f4564fbd9ea95f39d
criflow <crisflowbg11@gmail.com>
2016-06-22 20:26:21 -0400

    Update filtrado.cpp

--- filtrado.cpp
+++ filtrado.cpp
@@ -10,16 +10,21 @@
 
 using namespace std;
 
-
 void MostrarCarrosFiltrado(carro conchos[], int conchosTam, pasajero pasajeros[], int pasajerosTam, Servicio servs[], int ServiciosTam)
 {
-    char filtros;
+    char filtro;
     int id = -1;
-    int anoCarro1=0, anoCarro2=0;
+    rango ano;
     char marca[20] = {'0'}, propietario[20] = {'0'};
-    float pago1 = 0, pago2 = 0;
-    int dia1, mes1, ano1 = -1, hora1, min1, seg1, dia2, mes2, ano2 = -2, hora2, min2, seg2;
+    rango tiempoViaje, pago, total;
+    tiempo fecha1, fecha2;
+    string nombreFiltros[6] = {"Marca", "Año", "Tiempo de viaje", "Monto de pago", "Monto de total", "Fecha de viaje"};
+    bool filtrosBool[6];
+    void *filtros[7] = {&marca, &ano, &tiempoViaje, &pago, &total, &fecha1, &fecha2};
+
+
     MostrarPasajeros(pasajeros, pasajerosTam);
+
     do{
         printf("\n-Id del pasajero: ");
         cin >> id;
@@ -28,11 +33,11 @@ void MostrarCarrosFiltrado(carro conchos[], int conchosTam, pasajero pasajeros[]
     }while(BuscarPasajero(pasajeros, pasajerosTam,id)==false);
     do{
     printf("\n-Desea agregar filtros? (s/n): ");
-    cin >> filtros;
-        if(filtros!='s' && filtros!='n')
+    cin >> filtro;
+        if(filtro!='s' && filtro!='n')
             printf("\nERROR: OPCIONES VALIDAS: s/n");
-    }while(filtros!='s' && filtros!='n');
-    if(filtros == 'n')
+    }while(filtro!='s' && filtro!='n');
+    if(filtro == 'n')
     {
         printf("--------- Carros en los que se ha montado el pasajero %d-----------", id);
         for(int i=0; i<ServiciosTam; i++)
@@ -44,126 +49,105 @@ void MostrarCarrosFiltrado(carro conchos[], int conchosTam, pasajero pasajeros[]
         getch();
         return;
     }
-    printf("\n-Introduzca los siguientes filtros o presione backspace para ignorar:");
-    printf("\n*Desea agregar un rango del a%co del modelo del vehiculo?  ", 164);
-    if(getche() != 8)
-    {
-        fflush(stdin);
-        printf("\n-Minimo: ");
-        cin >> anoCarro1;
-        do{
-        printf("-Maximo: ");
-        cin >> anoCarro2;
-        if(anoCarro2<anoCarro1 || anoCarro2 == anoCarro1)
-            printf("\nERROR: EL MAXIMO DEBE SER MAYOR QUE EL MINIMO\n");
-        }while(anoCarro2<anoCarro1 || anoCarro2 == anoCarro1);
-    }else
-        printf("***ignorar***");
 
-    printf("\n\n*Desea agregar la marca del carro?  ");
-    if(getche() != 8)
+ cout << "Presione espacio para seleccionar o otra letra para ignorar\n";
+    for(int i=0; i<6; i++)
     {
-        printf("\n-Marca: ");
-        cin >> marca;
+        cout << "Desea agregar un filtro de: " << nombreFiltros[i] << "? ";
+        if(getche() == 32)
+        {
+            filtrosBool[i] = true;
+            cout << "*SELECCIONADO*";
+        }
+        else
+        {
+            filtrosBool[i] = false;
+            cout << "*Ignorar*";
+        }
+        cout << endl;
     }
-    else
-        printf("***ignorar***");
-
-        printf("\n*Desea agregar un rango del monto con que pago?  ");
-    if(getche() != 8)
+        for (int i=0; i<2; i++)
     {
-        fflush(stdin);
-        printf("\n-Minimo: ");
-        cin >> pago1;
-        do{
-        printf("-Maximo: ");
-        cin >> pago2;
-        if(pago2<pago1 || pago2 == pago1)
-            printf("\nERROR: EL MAXIMO DEBE SER MAYOR QUE EL MINIMO\n");
-        }while(pago2<pago1 || pago2 == pago1);
-    }
-    else
-        printf("***ignorar***");
+        if(filtrosBool[i] == true)
+        {
+            cout << nombreFiltros[i] << ": ";
+            cin >> (char*)(filtros[i]);
+        }
 
-    printf("\n\n*Desea agregar el nombre de propietario?  ");
-    if(getche() != 8)
+    }
+    for (int i=2; i<5; i++)
     {
-        printf("\n-Propietario: ");
-        cin >> propietario;
+        if(filtrosBool[i] == true)
+        {
+        cout << nombreFiltros[i] << ": " << endl;
+        cout << "-Rango mayor: ";
+        cin >> (((rango*)(filtros[i]))->mayor);
+        cout << "-Rango menor: ";
+        cin >> (((rango*)(filtros[i]))->menor);
+        }
     }
-    else
-        printf("***ignorar***");
 
-    printf("\n*Desea agregar un rango de la fecha y hora del servicio?  ");
-    if(getche() != 8)
+    if(filtrosBool[5]==true)
+        printf("Ingrese los datos del rango para la fecha 1 y fecha 2 en el siguiente formato: \n");
+    for(int i=5; i<7; i++)
     {
-        fflush(stdin);
-        printf("\n-Fecha Minima: ");
-        printf("\n-Dia: ");
-        cin >> dia1;
-        printf("-Mes: ");
-        cin >> mes1;
-        printf("-Ano: ");
-        cin >> ano1;
-        printf("-Hora: ");
-        cin >> hora1;
-        printf("-Minuto: ");
-        cin >> min1;
-        printf("-Segundo: ");
-        cin >> seg1;
-        printf("\n-Fecha maxima: ");
-        printf("\n-Dia: ");
-        cin >> dia2;
-        printf("-Mes: ");
-        cin >> mes2;
-        printf("-Ano: ");
-        cin >> ano2;
-        printf("-Hora: ");
-        cin >> hora2;
-        printf("-Minuto: ");
-        cin >> min2;
-        printf("-Segundo: ");
-        cin >> seg2;
-    }
-    else
-        printf("***ignorar***");
+        if(filtrosBool[5]==true)
+        {
+            printf("Fecha %d:\n", i-4);
+            do
+            {
+                printf("(dia mes a%co) sin parentesis: ", 164);
+                cin >> (((tiempo*)(filtros[i]))->dia);
+                cin >> (((tiempo*)(filtros[i]))->mes);
+                cin >> (((tiempo*)(filtros[i]))->ano);
 
-    for(int i=0; i<ServiciosTam; i++)
+            }while((fecha1.ano*10000 + fecha1.mes * 100 + fecha1.dia)>99991231 || (fecha1.ano*10000 + fecha1.mes * 100 + fecha1.dia)<0);
+            do
+            {
+                printf("(hora minuto segundo) sin parentresis: ");
+                cin >> (((tiempo*)(filtros[i]))->hora);
+                cin >> (((tiempo*)(filtros[i]))->minuto);
+                cin >> (((tiempo*)(filtros[i]))->segundo);
+            }while((fecha1.hora*10000 + fecha1.minuto * 100 + fecha1.segundo) > 245959 || (fecha1.hora*10000 + fecha1.minuto * 100 + fecha1.segundo) < 0);
+        }
+    }
+    printf("\n-----------------------------------------------------------------\n");
+        for(int i=0; i<ServiciosTam; i++)
         {
             if(servs[i].idPasajero == id)
                 {
                     for(int j=0; j<conchosTam; j++)
+                    {
+                        if(strncmp(servs[i].fichaVehiculo, conchos[j].ficha, 4)==0)
                         {
-                            if(strncmp(servs[i].fichaVehiculo, conchos[j].ficha, 4)==0)
-                                {
-                                    if(anoCarro1!=anoCarro2 && (conchos[j].ano<anoCarro1 || conchos[j].ano>anoCarro2))
-                                    break;
-                                        else if(marca[0] != '0' && strncmp(conchos[j].marca, marca, 20)!=0)
-                                            break;
-                                                else if(pago1!=pago2 && (servs[i].pago<pago1 || servs[i].pago>pago2))
-                                                    break;
-                                                        else if(propietario[0] != '0' && strncmp(conchos[j].propietario, propietario, 20)!=0)
-                                                            break;
-                                                                else if(ano1>0 && ano2>0){
-                                                                        if(ano1!=ano2 && (servs[i].fecha.ano>=ano1 && servs[i].fecha.ano<=ano2))
-                                                                            MostrarCarro(conchos, conchosTam, servs[i].fichaVehiculo);
-                                                                        else if(ano1==ano2 && servs[i].fecha.ano == ano1 && (servs[i].fecha.mes>=mes1 && servs[i].fecha.mes<=mes2))
-                                                                            MostrarCarro(conchos, conchosTam, servs[i].fichaVehiculo);
-                                                                        else if(mes1==mes2 && servs[i].fecha.mes == mes1 && (servs[i].fecha.dia>=dia1 && servs[i].fecha.dia<=dia2))
-                                                                            MostrarCarro(conchos, conchosTam, servs[i].fichaVehiculo);
-                                                                        else if(dia1==dia2 && servs[i].fecha.dia == dia1 && (servs[i].fecha.hora>=hora1 && servs[i].fecha.hora<=hora2))
-                                                                            MostrarCarro(conchos, conchosTam, servs[i].fichaVehiculo);
-                                                                        else if(min1==min2 && servs[i].fecha.minuto == min1 && (servs[i].fecha.minuto>=min1 && servs[i].fecha.minuto<=min2))
-                                                                            MostrarCarro(conchos, conchosTam, servs[i].fichaVehiculo);
-                                                                        else if(servs[i].fecha.segundo>=seg1 && servs[i].fecha.segundo<=seg2)
-                                                                                MostrarCarro(conchos, conchosTam, servs[i].fichaVehiculo);
-                                                                }else MostrarCarro(conchos, conchosTam, servs[i].fichaVehiculo);
+                            bool aux = true;
+                            void *current[6] = {&conchos[j].marca, &conchos[j].ano, &servs[i].minutosDeViaje, &servs[i].pago, &servs[i].devuelta, &servs[i].fecha};
 
+                            for(int k=0; k<2; k++)
+                                {
+                                    if(filtrosBool[k]==true && strncmp(((char*)(current[k])), ((char*)(filtros[k])), 4)!=0)
+                                        aux = false;
                                 }
+                            for(int k=2; k<4; k++)
+                                {
+                                    if(filtrosBool[k]==true && (*(float*)(current[k])) < (((rango*)(filtros[k]))->menor) && (*(float*)(current[k])) > (((rango*)(filtros[k]))->mayor))
+                                        aux = false;
+                                }
+                            if(filtrosBool[5]==true)
+                            {
+                                aux = CompararFechas((*(tiempo*)(current[5])),(*(tiempo*)(filtros[5])), (*(tiempo*)(filtros[6])));
+                            }
+                            if(aux==true)
+                                    MostrarCarro(conchos, conchosTam, servs[i].fichaVehiculo);
                         }
+                    }
                 }
         }
+
     getch();
+    return;
+
+
     return;
 }
 
